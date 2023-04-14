a752de05cf5313708aa85f98e91fad03c70cb837
Lokesh S <lokesh.selvam@zohocorp.com>
2023-04-14 21:33:39 +0530

    login page changed

--- NAS Software/registeration.py
+++ NAS Software/registeration.py
@@ -1,32 +1,25 @@
+import json
 import streamlit as st
 
-def signup():
-    st.title("Sign Up")
-
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    confirm_password = st.text_input("Confirm Password", type="password")
-
-    if st.button("Sign Up"):
-        if password == confirm_password:
-            # Your signup logic here
-            st.success("You have successfully signed up!")
-        else:
-            st.error("Passwords do not match. Please try again.")
-
-def login():
-    st.title("Login")
-
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-
-    if st.button("Login"):
-        # Your login logic here
-        st.success("You have successfully logged in!")
-
-option = st.sidebar.selectbox("Select an option", ["Login", "Sign Up"])
-
-if option == "Login":
-    login()
-else:
-    signup()
+# Define the Streamlit app
+def main():
+    st.title("Login / Register")
+    mode = st.radio("Select mode:", ("Login", "Register"))
+    username = st.text_input("Username:")
+    password = st.text_input("Password:", type="password")
+    if st.button(mode):
+        # Load the JSON data from the file
+        with open("users.json", "r") as f:
+            users = json.load(f)
+        if mode == "Login":
+            # Check if the entered username and password match a record in the JSON data
+            if username in users and users[username]["password"] == password:
+                st.success("Logged in!")
+            else:
+                st.error("Invalid username or password")
+        elif mode == "Register":
+            # Add the new user to the JSON data and save it to the file
+            users[username] = {"password": password}
+            with open("users.json", "w") as f:
+                json.dump(users, f)
+            st.success("Registered!")
