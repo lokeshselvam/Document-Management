dbcaf806dae652525ce6fa07131d0c16d388a819
lokeshselvam <lokeshwaran186@gmail.com>
2023-04-17 06:49:55 +0530

    working model

---
+++ pages/filemanager.py
@@ -0,0 +1,54 @@
+import os
+import streamlit as st
+
+# Define the Streamlit app
+def main():
+    st.set_page_config(
+        page_title="File Manager",
+        page_icon="👋",
+    )
+    st.title("File Manager")
+    file_type = st.selectbox("Select file type:", ("All", "Image", "Text", "PDF"))
+    files = get_files(file_type)
+    file = st.selectbox("Select a file:", files)
+    if file:
+        download_button(file)
+
+    # File upload section
+    st.header("Upload a File")
+    uploaded_file = st.file_uploader("Choose a file to upload")
+    if uploaded_file is not None:
+        save_uploaded_file(uploaded_file)
+
+def get_files(file_type):
+    """Get a list of files of the specified type."""
+    files = []
+    for filename in os.listdir("."):
+        if file_type == "All":
+            files.append(filename)
+        elif file_type == "Image" and filename.endswith((".png", ".jpg", ".jpeg")):
+            files.append(filename)
+        elif file_type == "Text" and filename.endswith(".txt"):
+            files.append(filename)
+        elif file_type == "PDF" and filename.endswith(".pdf"):
+            files.append(filename)
+    return files
+
+def download_button(file):
+    """Display a download button for the specified file."""
+    with open(file, "rb") as f:
+        contents = f.read()
+    st.download_button(
+        label=f"Download {file}",
+        data=contents,
+        file_name=file
+    )
+
+def save_uploaded_file(uploaded_file):
+    """Save the uploaded file to the current directory."""
+    with open(uploaded_file.name, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    st.success(f"Saved {uploaded_file.name}")
+    
+if __name__=="__main__":
+	main()
---
+++ registeration.py
@@ -0,0 +1,35 @@
+import json
+import streamlit as st
+
+# Define the Streamlit app
+def main():
+    st.set_page_config(
+        page_title="Login page",
+        page_icon="👋",
+    )
+    st.sidebar.success("Select the above page")
+
+    st.title("Login / Register")
+    mode = st.radio("Select mode:", ("Login", "Register"))
+    username = st.text_input("Username:")
+    password = st.text_input("Password:", type="password")
+    if st.button(mode):
+        # Load the JSON data from the file
+        with open("users.json", "r") as f:
+            users = json.load(f)
+        if mode == "Login":
+            # Check if the entered username and password match a record in the JSON data
+            if username in users and users[username]["password"] == password:
+                st.success("Logged in!")
+                html = '<meta http-equiv="refresh" content="0; url=/filemanager" />'
+                st.write(html, unsafe_allow_html=True)
+            else:
+                st.error("Invalid username or password")
+        elif mode == "Register":
+            # Add the new user to the JSON data and save it to the file
+            users[username] = {"password": password}
+            with open("users.json", "w") as f:
+                json.dump(users, f)
+            st.success("Registered!")
+if __name__=="__main__":
+	main()
