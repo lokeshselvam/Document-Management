4ed478a70eb3faf49de6388d46938ffec279e2da
Lokesh S <lokesh.selvam@zohocorp.com>
2023-04-17 14:05:42 +0530

    issue fixed by streamlit team

--- registeration.py
+++ registeration.py
@@ -1,5 +1,31 @@
 import json
 import streamlit as st
+from streamlit.components.v1 import html
+
+def nav_page(page_name, timeout_secs=3):
+    nav_script = """
+        <script type="text/javascript">
+            function attempt_nav_page(page_name, start_time, timeout_secs) {
+                var links = window.parent.document.getElementsByTagName("a");
+                for (var i = 0; i < links.length; i++) {
+                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
+                        links[i].click();
+                        return;
+                    }
+                }
+                var elasped = new Date() - start_time;
+                if (elasped < timeout_secs * 1000) {
+                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
+                } else {
+                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
+                }
+            }
+            window.addEventListener("load", function() {
+                attempt_nav_page("%s", new Date(), %d);
+            });
+        </script>
+    """ % (page_name, timeout_secs)
+    html(nav_script)
 
 # Define the Streamlit app
 def main():
@@ -23,8 +49,7 @@ def main():
                     st.success("Logged in!")
                     st.session_state['username'] = username
                     st.write(st.sidebar)
-                    # html = '<meta http-equiv="refresh" content="0; url=/FileManager" />'
-                    # st.write(html, unsafe_allow_html=True)
+                    nav_page("FileManager")
                 else:
                     st.error("Invalid username or password")
             elif mode == "Register":
